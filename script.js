5a1c29a9cb95d4656d085fe6d838eb8d86476a3f
Wafa AIT BAHA <w.aitbaha5493@uca.ac.ma>
2026-05-01 14:19:49 +0200

    dark theme

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ const header = document.querySelector('header');
         section.classList.add('reveal-effect');
         observer.observe(section);
       });
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line' // Icon to show when in Dark Mode
+
+// 1. Check if user previously chose a theme
+const selectedTheme = localStorage.getItem('selected-theme')
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+}
+
+// 2. Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark theme class
+    document.body.classList.toggle(darkTheme)
+    
+    // Save the theme choice in local storage
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    themeButton.classList.toggle('ri-moon-line')
+    themeButton.classList.toggle('ri-sun-line')
+})
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
